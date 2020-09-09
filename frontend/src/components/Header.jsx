76b7dd2e07c99987d1f27f335a2ca623ef0fca90
Salil Naik <salil.naik27@gmail.com>
2020-09-10 05:05:25 +0530

    initial frontend setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "../images/feminist_bible_logo.png";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="grid-container">
+          <div className="grid-x grid-margin-x">
+            <div className="cell large-3">
+              <img src={logo} alt="feminist bible logo" className="header__logo"/>
+            </div>
+
+            <div className="cell large-9">
+              <nav className="nav">
+                <ul className="nav__item-container">
+                  <li className="nav__item"><a href="">Stories</a></li>
+                  <li className="nav__item"><a href="">Organizations</a></li>
+                  <li className="nav__item"><a href="">About</a></li>
+                  <li className="nav__item"><a href="">Login</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
