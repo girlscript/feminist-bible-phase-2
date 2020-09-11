be059a46d57421d4bbfcf70c9a390d14f4d07d46
priyanshisharma <priyanshi417@gmail.com>
2020-09-11 19:01:23 +0530

    minor linting

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,26 +11,27 @@ class Header extends Component {
             <div className="cell large-3">
               <img src={logo} alt="feminist bible logo" className="header__logo"/>
             </div>
-
             <div className="cell large-9">
               <nav className="nav">
                 <ul className="nav__item-container">
                   <li className="nav__item"><a href="">Stories</a></li>
                   <li className="nav__item"><a href="">Organizations</a></li>
                   <li className="nav__item"><a href="">About</a></li>
-                  {this.props.isSignedIn?  
-                    <>
-                      <li className="nav__item">
-                        <button className="button__big" style={{textTransform: "capitalize"}}>
-                          Write a Story
-                        </button>
-                      </li>
-                      <li className="nav__item" data-testid="user-avatar">
-                        <img src={this.props.avatarSrc} class="avatar" alt="signed-in user"/>
-                      </li> 
-                    </>:
-                  <li className="nav__item"><a href="">Login</a></li>
-                  }
+                    {
+                      this.props.isSignedIn?  
+                        <> 
+                          <li className="nav__item">
+                            <button className="button__big" style={{textTransform: "capitalize"}}>
+                              Write a Story
+                            </button>
+                          </li>
+                          <li className="nav__item" data-testid="user-avatar">
+                            <img src={this.props.avatarSrc} class="avatar" alt="signed-in user"/>
+                          </li> 
+                        </>
+                        :
+                        <li className="nav__item"><a href="">Login</a></li>
+                    }
                 </ul>
               </nav>
             </div>
