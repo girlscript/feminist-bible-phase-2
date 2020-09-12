77c25cf9fe96f9213eecba58ee88b81066c417ff
Abdus <mail@abdus.xyz>
2020-09-12 14:30:42 +0530

    add swagger

--- fb-phase2-backend/app.js
+++ fb-phase2-backend/app.js
@@ -1,11 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const authRouter = require("./routes/authRoutes");
+const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const authRouter = require('./routes/authRoutes');
+const swaggerDefinition = require('./config/swaggerDefinition');
 
 const app = express();
+const swaggerSpec = swaggerJSDoc({
+  swaggerDefinition: swaggerDefinition,
+  apis: ['./routes/*.js'],
+});
 
-
-
-app.use("/api/auth", authRouter);
+// routes
+app.use('/api/auth', authRouter);
+app.use(
+  '/docs',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerSpec, {
+    customCss: '.swagger-ui .topbar { display: none }',
+  })
+);
 
 module.exports = app;
