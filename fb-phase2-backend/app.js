b8d9629a4f3ca2d1ba81bac47f6c3ff185b0112f
unknown <rrmuddinagiri@gmail.com>
2020-09-14 00:34:58 +0530

    fix conflicts

--- fb-phase2-backend/app.js
+++ fb-phase2-backend/app.js
@@ -1,14 +1,14 @@
-const express = require("express");
+// const express = require("express");
 const mongoose = require("mongoose");
 
-const authRouter = require("./routes/authRoutes");
+// const authRouter = require("./routes/authRoutes");
 const storyRouter = require("./routes/storyRoutes")
 
-const app = express();
+// const app = express();
 
 app.use(express.json())
  
-app.use("/api/auth", authRouter);
+// app.use("/api/auth", authRouter);
 app.use("/api/story", storyRouter)
 
-module.exports = app;
+// module.exports = app;
