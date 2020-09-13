3b6cd84cfd19d91a4b433f95acfbb5b02a9ed18e
Kushagra Johari <skjkushagra1309@gmail.com>
2020-09-14 02:23:46 +0530

    resolve conflict app.js

--- fb-phase2-backend/app.js
+++ fb-phase2-backend/app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./routes/authRoutes");
 
-const app = express();
 
-app.use(express.json());
 
 app.use("/api/auth", authRouter);
 
