6114deca71cef8d84464731217945fc63a6de6df
Sonali singh <sonali122000@gmail.com>
2020-09-14 08:01:32 +0530

    modified classnames

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './components/Header.jsx';
-
+import BlogCard from './components/BlogCard.jsx';
 
 function App() {
   return (
     <div>
       <Header />
-     
+      <BlogCard />
     </div>
   );
 }
--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -7,19 +7,21 @@ class BlogCard extends Component {
     render() {
         return (
             <div className="card">
-                <div className="card-container">
-                    <img src= {blog_img} className="card-container__img" />
+               
+                   <div className=" blog__card ">
+                    <img src= {blog_img} className="blog__card__img" />
                     </div>
-                    <div className="card-container">
-                        <h3>QTPOC In Comics: “Bingo Love” Depicts Black Queer Women’s Romance</h3>
+                    <div className=" blog__card ">
+                        <h3 className="blog__card__detail">QTPOC In Comics: “Bingo Love” Depicts Black Queer Women’s Romance</h3>
                     </div>
-
-                    <div className="footer" >
-                        <div >
-                       <p className=" footer__text footer__text--large">Rashmeet Kaur</p>
-                       <p className=" footer__text footer__text--small">12 Aug 2020</p>
+                     
+                    
+                    <div className="author__card" >
+                        <div className="author__card__detail" >
+                       <p className="author__name">Rashmeet Kaur</p>
+                       <p className="author__date">12 Aug 2020</p>
                         </div>
-                        <div  className="footer__img">
+                        <div  className="author__card__img">
                             <img  src={author_img}/>
                         </div>
                     
