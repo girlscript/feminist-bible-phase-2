62bc5db6da0e36346f3d23cd243315d7c9329079
Salil Naik <salil.naik27@gmail.com>
2020-09-14 08:08:45 +0530

    fix: input component

--- frontend/src/components/input.jsx
+++ frontend/src/components/input.jsx
@@ -5,7 +5,7 @@ class Input extends Component {
   render() {
     return (
       <div className="form__element-container">
-        <label className="form__label>Email Address</label>
+        <label className="form__label required">Email Address</label>
         <input className="form__input-field" type="text" placeholder="username@example.com" />
       </div>       
     );
