ce24482391bc7888672c96e9c893e28a5ac085d8
pkpsyder007 <pkpsyder007@gmail.com>
2020-09-15 15:56:57 +0530

    Add Reusable input component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import Header from './components/Header.jsx';
 import avatar from "./images/avatar.png";
+import { WithPasswordStrength, Input } from './components/input.jsx';
 
 function App() {
   return (
     <div>
       <Header isSignedIn={true} avatarSrc={avatar} />
+      {/* Delete this just for demonstration purpose */}
+      <div style={{ width: '500px', margin: 'auto' }}>
+        <Input
+          label="Name"
+          type="text"
+          placeholder="Name"
+          error="Error can also be passed down based on the event"
+          onChange={(e) => {/*do someething with the event in this fuction */ }}
+        />
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Email Address"
+          error=""
+
+        />
+        <WithPasswordStrength
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          error=""
+          onChange={(e) => {/*do someething with the event in this fuction */ }}
+        />
+        <Input
+          label="Confirm Password"
+          type="password"
+          placeholder="Enter your password again"
+          error=""
+          onChange={(e) => {/*do someething with the event in this fuction */ }}
+        />
+      </div>
+      {/* Delete upto here */}
     </div>
   );
 }
--- frontend/src/components/input.jsx
+++ frontend/src/components/input.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from "react";
 import '../css/components/input.scss';
 
-// Error for testing purpose
-// for displaying error state
-// const error = 'Error Mesaage from somewhere.';
-// for not displaying error
-const error = '';
 
 class Input extends Component {
-
-  handlePasswordChange = (e) => {
-    if(this.props.handlePasswordStrength) {
-      this.props.handlePasswordStrength(e.target.value);
-    }
-  }
-
   render() {
+    const { label, error, type, placeholder, onChange } = this.props;
     return (
       <div className="form__element-container">
-        <label className="form__label required">Email Address</label>
-        <input className={`form__input-field ${error? 'error' : ''}`} type="text" placeholder="username@example.com" onChange={this.handlePasswordChange} />
+        <label className="form__label required">{label}</label>
+        <input className={`form__input-field ${error? 'error' : ''}`} type={type} placeholder={placeholder} onChange={onChange} />
         {error && <span className="form__input-error">{error}</span>}
       </div>       
     );
@@ -31,29 +20,48 @@ class WithPasswordStrength extends Component {
   // vary this strength property between 1 to 4 to see changes in progress bar
   state = {
     password: '',
-    strength: '4'
+    strength: '0'
   }
   
-  handlePasswordStrength = password => {
-    this.setState({password});
+  handlePasswordStrength = (e) => {
+    this.setState({password: e.target.value});
+    this.props.onChange(e);
+    const { password } = this.state;
+    if(password.length <= 4) {
+      this.setState({strength: '1'});
+    } else if(password.length > 4 && password.length < 6) {
+      this.setState({strength: '2'});
+    }
+    else if(password.length >= 6 && password.length < 8) {
+      this.setState({strength: '3'});
+    } else if(password.length >= 8) {
+      this.setState({strength: '4'});
+    } else {
+      this.setState({strength: '1'});
+    }
   }
 
   render() {
     const {strength} = this.state;
     const bgcolors = ["red", "orange", "yellow", "green"];
     const strengths = ["Week", "Ok", "Medium", "Strong"]
-
+    const { label, type, error, placeholder } = this.props;
     return (
       <div>
-        <Input handlePasswordStrength={this.handlePasswordStrength} />
-        <div className="form__input-strength">
+        <Input 
+          label={label}
+          type={type}
+          placeholder={placeholder}
+          error={error}
+          onChange={this.handlePasswordStrength}
+        />
+        { strength > 0  ? (<div className="form__input-strength">
           <p className="result" style={{color: bgcolors[strength - 1]}}>{strengths[strength - 1]}</p>
           <div className="meter">
             <div className="strength" style={{ width: `${25*strength}%`, backgroundColor: bgcolors[strength - 1]}}>
-              
             </div>
           </div>
-        </div>
+        </div>) : null }
       </div>
     );
   }
