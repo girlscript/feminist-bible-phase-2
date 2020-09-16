a85625740d023db37dd8ac27bddc9478ff116090
Sshivani-12 <shivaniwakde1211@gmail.com>
2020-09-16 14:17:17 +0530

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import CodeOfConduct from './components/CodeOfConduct.jsx';
 import Header from './components/Header.jsx';
+import avatar from "./images/avatar.png";
+
+//delete this
+import BlogCard from "./components/BlogCard.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <CodeOfConduct/>
+=======
+    <div style={{background: 'pink', height:'100vh'}}>
+      <Header isSignedIn={true} avatarSrc={avatar}/>
+
+      {/* delete this */}
+      <div className="grid-container" style={{marginTop: '50px', marginBottom: '100px'}}>
+        <div className="grid-x grid-margin-x">
+          <div className="cell large-4">
+            <BlogCard />
+          </div>
+          <div className="cell large-4">
+            <BlogCard />
+          </div>
+          <div className="cell large-4">
+            <BlogCard />
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
