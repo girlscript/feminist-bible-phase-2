e5b7022f1b53fe99f36116c1c26fce6cf57f495f
joharikushagra <57484457+joharikushagra@users.noreply.github.com>
2020-09-17 07:48:05 +0530

    Created Footer (#59)
    * initial commit
    
    * created footer
    
    * made suggested changes
    
    * made changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import avatar_image from "./images/avatar.png";
 
 function App() {
   return (
-    <div >
-      <Header isSignedIn={true} avatarSrc={avatar_image}/>
+    <div>
+      {/* <Header isSignedIn={true} avatarSrc={avatar}/> */}
+     <Footer />
+    
+      {/* delete this */}
     </div>
   );
 }
 
 export default App;
+
