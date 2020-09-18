50a77351f9f71e8e047727c44fa3ab175142f216
shwetalsoni <57187745+shwetalsoni@users.noreply.github.com>
2020-09-18 08:41:38 +0530

    Made required changes as per suggestions (#60)
    * Issue  #48 Code of Conduct page
    
    * issue #48 codeofconduct
    
    * changed overall CodeOfConduct.jsx and respective scss file
    
    * changed card 1, card 2 and card3 to card ...block1,block2, block3 to guidelines-card....also named the images properly
    
    * refactored card section
    
    * added wrap class in hero section
    
    Co-authored-by: Sshivani-12 <shivaniwakde1211@gmail.com>
    Co-authored-by: Salil Naik <salil.naik27@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CodeOfConduct from './components/CodeOfConduct.jsx';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import avatar_image from "./images/avatar.png";
