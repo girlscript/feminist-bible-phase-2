55c74c94b4c1f665d37d40cad2c48e6bc3b8ac30
priyanshisharma <priyanshi417@gmail.com>
2020-09-20 13:55:12 +0530

    feat: Featured Stories Section

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -36,71 +36,74 @@ class Home extends Component {
                             </div> 
                 </div>
             </div>
-            <div>
-                <div className="importanceOfDiversity">
+            
             </div>
-        </div>
-        </div>
-        <div style={{background:"linear-gradient(180deg, #FFFDF6 0%, #FFEBF3 100%)"}}>
-            <div className="importanceOfDiversity ">
-                <div className="cell small-7 large-offset-1">
-                    <h2>
-                        Importance of Diversity
-                    </h2>
-                    <br/>
-                </div>
-                
-                <div className="grid-x grid-margin-x">
-
-                <div className="cell large-5 medium-6 large-offset-1">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                        do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-                        irure dolor in reprehenderit in voluptate velit esse cillum dolore
-                        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                        , sunt in culpa qui officia deserunt mollit anim id est laborum." 
-                    </p>
-                    <br/>
-                    <p>
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
-                        in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+            <div style={{background:"linear-gradient(180deg, #FFFDF6 0%, #FFEBF3 100%)"}}>
+                <div className="importanceOfDiversity ">
+                    <div className="cell small-7 large-offset-1">
+                        <h2>
+                            Importance of Diversity
+                        </h2>
+                        <br/>
                     </div>
-                        <div className="cell large-5 medium-6 ">
+                    <div className="grid-x grid-margin-x">
+                        <div className="cell large-5 medium-6 large-offset-1">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat c
-                                upidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                                do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                                , sunt in culpa qui officia deserunt mollit anim id est laborum." 
                             </p>
                             <br/>
-                        </div> 
+                            <p>
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
+                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+                                in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                            <div className="cell large-5 medium-6 ">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat c
+                                    upidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                <br/>
+                            </div> 
+                        </div>
+                    </div> 
+                    <br/>       
                 </div>
-                </div>        
-            </div>
-            <div style={{background:"#FFEBF3"}}>
-                <div className="feature">
-                    <div className="cell small-7">
-                    <h2>
-                            Featured Stories
+                
+                <div style={{background:"#FFEBF3"}}>
+                    <div className="feature">
+                        <div className="cell small-7">
+                        <h2>
+                                Featured Stories
                         </h2>
                         <h6 style={{color:"#6E6D7A"}}>
                             Read the impact stories from around the world
                         </h6>
-                        <div style={{display:"inline-block"}}>
-                            <BlogCard/>
-                            <BlogCard/>
-                            <BlogCard/>
-                        </div>
+                        <br/>
+                        <ul className="list__item-container large-offset-1">
+                            <li className="list__item">
+                                <BlogCard/>
+                            </li>
+                            <li className="list__item">
+                                <BlogCard/>
+                            </li>
+                            <li className="list__item">
+                                <BlogCard/>
+                            </li>
+                        </ul>
                     </div>
                         
-                    
                 </div>
-                </div>
+            </div>
           </>
       )
     }
