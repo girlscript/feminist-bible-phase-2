03cd9fd0e2bac41a44be24a5bacdfebf06fdd7fc
Ruchika Muddinagiri <rrmuddinagiri@gmail.com>
2020-09-23 11:34:21 +0530

    approval system

--- fb-phase2-backend/controllers/adminController.js
+++ fb-phase2-backend/controllers/adminController.js
@@ -3,83 +3,83 @@ const Organization = require('../database/models/orgModel');
 //get all approval requests
 //all organizations where organization model has approved:false and declined:false
 exports.approvalRequests = async (req, res) => {
-    try{
-        const orgs = await Organization.find({declined: false, approved:false})
-        if(!orgs){
-            return res.status(400).json({
-                message: "Organizations requiring approval not found!"
-            })
-        }
-        res.status(200).json({
-            message: "Success",
-            data: orgs
-        })
-    }catch(error){
-        res.status(400).json({
-            message: 'Error!',
-          }); 
+  try {
+    const orgs = await Organization.find({ declined: false, approved: false });
+    if (!orgs) {
+      return res.status(400).json({
+        message: 'Organizations requiring approval not found!',
+      });
     }
-     
+    res.status(200).json({
+      message: 'Success',
+      data: orgs,
+    });
+  } catch (error) {
+    
+    res.status(400).json({
+      message: 'Error!',
+    });
+  }
 };
 
 //approve a request
 //update organization model for that particular organization approved:true and declined:false
 exports.approveRequest = async (req, res) => {
-    try{
-        let org = await Organization.findById({_id: req.params.orgid})    
-        if(org.approved != true && org.declined != true){
-            org = await Organization.findByIdAndUpdate({_id: req.params.orgid}, {approved:true}, 
-                {
-                    new: true,
-                    runValidators: true
-                })
-            res.status(200).json({
-                message: "Successfully approved!",
-                data:{
-                    organization: org
-                }
-            })
-        }else{
-            res.status(400).json({
-                message: "Cannot approve!"
-            }) 
+  try {
+    let org = await Organization.findById({ _id: req.params.orgid });
+    if (org.approved != true && org.declined != true) {
+      org = await Organization.findByIdAndUpdate(
+        { _id: req.params.orgid },
+        { approved: true },
+        {
+          new: true,
+          runValidators: true,
         }
-        
-               
-    }catch(error){
-        res.status(400).json({
-            message: 'Error!',
-          });
+      );
+      res.status(200).json({
+        message: 'Successfully approved!',
+        data: {
+          organization: org,
+        },
+      });
+    } else {
+      res.status(400).json({
+        message: 'Cannot approve!',
+      });
     }
-    
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error!',
+    });
+  }
 };
 
 //decline a request
 //update org model for that particular org approved:false and declined:true
-exports.declineRequest = async(req, res)=>{
-    try{
-        let org = await Organization.findById({_id:req.params.orgid})
-        if(org.approved != true && org.declined!=true){
-            org = await Organization.findByIdAndUpdate({_id: req.params.orgid}, {declined:true}, 
-                {
-                    new: true,
-                    runValidators: true
-                })
-            res.status(200).json({
-                message: "Successfully declined!",
-                data: org
-            })
-        }else{
-            res.status(400).json({
-                message: "Cannot decline!"
-            })
+exports.declineRequest = async (req, res) => {
+  try {
+    let org = await Organization.findById({ _id: req.params.orgid });
+    if (org.approved != true && org.declined != true) {
+      org = await Organization.findByIdAndUpdate(
+        { _id: req.params.orgid },
+        { declined: true },
+        {
+          new: true,
+          runValidators: true,
         }
-        
-    }catch(error){
-        res.status(400).json({
-            message: "Error!"
-        })
+      );
+      res.status(200).json({
+        message: 'Successfully declined!',
+        data: org,
+      });
+    } else {
+      res.status(400).json({
+        message: 'Cannot decline!',
+      });
     }
-    
-}
-
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error!',
+    });
+  }
+};
