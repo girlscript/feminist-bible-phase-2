5f4c68bccaa76f1cfb0f3c47afe41f3933473722
Shrishti Goyal <shrishtigoyal2608@gmail.com>
2020-09-26 01:57:11 +0530

    updated orgController

--- fb-phase2-backend/controllers/orgController.js
+++ fb-phase2-backend/controllers/orgController.js
@@ -39,3 +39,26 @@ exports.createOrganization = async (req, res) => {
     },
   });
 };
+exports.updateOrganisation = async(req,res) =>{
+  try{
+    const org_id = req.param.OrgId;
+    const org = await Org.findOneAndUpdate({_id : org_id},req.body,{
+      new: true ,
+      runValidators: true,
+
+    });
+    if(!org)
+      throw new NotFoundError('Organisation with ${org_id} could not be found');
+
+    return res.json({message: 'success',data: org});
+  } catch (err){
+    const err_code = err.err_code
+      ? err.code >= 100 && err.code <=599
+        ? err.code 
+        : 500
+      : 500;
+    res
+      .status(err_code)
+      .json({message: err.message || 'Internal Server Error'});
+  }
+}; 
