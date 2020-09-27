c773400f1dc86d02f24e5ca2b7e7343e28bf365d
Gaurav Das <gauravdas014@gmail.com>
2020-09-27 23:10:37 +0530

    update org model

--- fb-phase2-backend/database/models/orgModel.js
+++ fb-phase2-backend/database/models/orgModel.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 
+/**
+ * @swagger
+ * definitions:
+ *    Org:
+ *     properties:
+ *      name:
+ *        type: string
+ *      email:
+ *         type: string
+ *        unique: true
+ *      phone:
+ *        type: number
+ *        unique: true
+ */
+
 const orgSchema = new mongoose.Schema(
   {
     name: {
@@ -7,10 +22,21 @@ const orgSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
     image: {
       type: String,
-      required: true,
     },
+    admin: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
     description: {
       type: String,
       required: true,
