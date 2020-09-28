32ce012e1d4b406c9db64865b82b63daa0ea16d3
Ruchika Muddinagiri <rrmuddinagiri@gmail.com>
2020-09-28 11:36:22 +0530

    changes

--- fb-phase2-backend/controllers/adminController.js
+++ fb-phase2-backend/controllers/adminController.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-const Org = require('../database/models/orgModel');
-=======
 const Organization = require('../database/models/orgModel');
 const Story = require('../database/models/storyModel')
 const User = require('../database/models/userModel')
 const mongoose = require('mongoose')
->>>>>>> 896a5207a939e7767976ef29deeb6ef4e1b21135
 
 //get all approval requests
 //all Orgs where Org model has approved:false and declined:false
@@ -90,79 +86,3 @@ exports.declineRequest = async (req, res) => {
   }
 };
 
-//approve a story request
-exports.approveStoryRequest = async (req, res) => {
-  try{
-    let story = await Story.findById({ _id: req.params.storyid });
-    if (story.approved != true && story.declined != true) {
-      story = await Story.findByIdAndUpdate(
-        { _id: req.params.storyid },
-        { approved: true },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-      res.status(200).json({
-        message: 'Successfully approved!',
-        data: story,
-      });
-    } else {
-      res.status(400).json({
-        message: 'Cannot approve!',
-      });
-    }
-  } catch (error) {
-    res.status(400).json({
-      message: 'Error!',
-    });
-  }
-}
-//decline a story request
-exports.declineStoryRequest = async (req, res) => {
-  try{
-    let story = await Story.findById({ _id: req.params.storyid });
-    if (story.approved != true && story.declined != true) {
-      story = await Story.findByIdAndUpdate(
-        { _id: req.params.storyid },
-        { declined: true },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-      res.status(200).json({
-        message: 'Successfully declined!',
-        data: story,
-      });
-    } else {
-      res.status(400).json({
-        message: 'Cannot decline!',
-      });
-    }
-  } catch (error) {
-    res.status(400).json({
-      message: 'Error!',
-    });
-  }
-}
-
-//get all story approval requests
-exports.getAllStoryApprovalRequests = async (req, res) => {
-  try{
-    Story.aggregate([
-      { $match: { approved: false, declined: false}},
-      { $group: {_id: "$author", "stories":{$push: "$$ROOT"}}}
-    ]).exec( function(err, stories){
-            res.status(200).json({
-              message: "Successfully retrieved story approval requests user-wise",
-              stories: stories
-            })
-      })
-  } catch (error) {
-    res.status(400).json({
-      message:"Error!",
-      error: error
-    })
-  }
-}
