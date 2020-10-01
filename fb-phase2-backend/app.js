f200510e341cb6172c4726cf43414bd61639bee6
Gaurav Das <gauravdas014@gmail.com>
2020-10-01 19:02:52 +0530

    add express-rate-limit to prevent failure in the api

--- fb-phase2-backend/app.js
+++ fb-phase2-backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const authRouter = require('./routes/authRoutes');
 const storyRouter = require('./routes/storyRoutes');
 const orgRouter = require('./routes/orgRoutes');
 const adminRouter = require('./routes/adminRoutes');
-const userRouter=require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 app.use(express.json());
+
+// express rate limiter
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP address',
+});
+
+// using express rate limit to every request that hit "/"
+app.use('/', limiter);
 
 // routes
 app.use('/api/auth', authRouter);
