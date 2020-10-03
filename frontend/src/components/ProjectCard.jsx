19c652a04ba7e978dd844cbc0993b1bd13ebfdc0
priyanshisharma <priyanshi417@gmail.com>
2020-10-03 15:42:55 +0530

    Update project card

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -5,10 +5,18 @@ import mail_gun_logo from "../images/mailgunlogo.png";
 class ProjectCard extends Component {
   render() {
     return (
-      <div className="project-card__container">
-          <div className="grid-container">
-                <div className="grid-x grid-margin-x">
-                    <div className="cell large-4">
+      <div className="">
+          <div className="grid-container ">
+                <div 
+                    className="grid-x grid-margin-x" 
+                    style=
+                        {{ boxShadow: "0px 0px 14px rgba(0, 0, 0, 0.1)" , 
+                        backgroundColor: "#F4F4F4"
+                    }}>
+                    <div 
+                        className="cell small-4 project-card__imgcontainer"
+                        style={{margin:"0px"}}
+                        >
                             <img 
                                 src={mail_gun_logo}
                                 className="blog-card__img"
@@ -18,7 +26,12 @@ class ProjectCard extends Component {
                                 }}
                             />
                     </div>
-                    <div className="cell large-8 blog-card__text">
+                    <div 
+                        className="cell small-8 
+                            project-card__textcontainer 
+                            project-card__text" 
+                        style={{margin:"0px"}}
+                        >
                         <h2>
                             Mailgun Auto Enhancement 
                         </h2>
