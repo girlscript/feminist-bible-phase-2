6e9d8c7c7932381a68ae310fb9d4182a8fe03e65
priyanshisharma <priyanshi417@gmail.com>
2020-10-20 14:14:00 +0530

    add'

--- frontend/src/components/OrganisationCard.jsx
+++ frontend/src/components/OrganisationCard.jsx
@@ -1,39 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import "../css/components/organisationCard.scss";
-import org_logo from "../images/amazon.png";
-
-class OrganisationCard extends Component {
-    render(){
-        return (
-            <div className="orgCard__container">
-                <div className="orgCard__logo">
-                    <img
-                        src = {org_logo}
-                        className="orgCard__logoimg"/>
-                </div>
-                <div className="orgCard__text-container">
-                    <p className="orgCard__heading-text">
-                        Amazon Open Source
-                    </p>
-                    <div className="orgCard__subtext">
-                        <p>
-                            Authorization library that supports 
-                            access control models like ACL, RBAC, ABAC.
-                        </p>
-                        <a>
-                            <u>
-                                Learn more
-                            </u>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-export default OrganisationCard;
-=======
 import org_logo from "../images/homepage/partners-logo/amazon.png";
 
 class OrganisationCard extends Component {
@@ -58,4 +23,3 @@ class OrganisationCard extends Component {
   }
 }
 export default OrganisationCard;
->>>>>>> f68093d311867f4adce6db88de5c0631260d8d8f
