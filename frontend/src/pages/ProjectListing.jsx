6d9d80e3c55054b4dd78142640ca3b0c6132c3ad
priyanshisharma <priyanshi417@gmail.com>
2020-10-22 00:08:44 +0530

    feat: map project list

--- frontend/src/pages/ProjectListing.jsx
+++ frontend/src/pages/ProjectListing.jsx
@@ -21,29 +21,29 @@ class ProjectListing extends Component {
       },
     ];
 
+    let convertedData = Object.keys(data).map(igKey => {
+        return [...Array(data[igKey])].map((_, i) => {
+            return (
+              <div className="cell large-12">
+                <div className="ProjectListing__card">
+                  <ProjectCard className="ProjectListing__card" title={data[igKey].title} text={data[igKey].text} />
+                </div>
+              </div>
+            )}); 
+    }).reduce((arr, el) => {
+      return arr.concat(el)
+    },[]);
+
     return (
       
         <div className="grid-container">
           <div className="cell large-7">
             <h1 className="ProjectListing__title">All Projects</h1>
           </div>
-
           <div className="grid-x grid-margin-x">
             <div className="cell large-12">
-              <div className="ProjectListing__card">
-                <ProjectCard title={data[0].title} text={data[0].text} />
-              </div>
-            </div>
-
-            <div className="cell large-12">
-              <div className="ProjectListing__card">
-                <ProjectCard title={data[1].title} text={data[1].text} />
-              </div>
-            </div>
-
-            <div className="cell large-12">
-              <div className="ProjectListing__card">
-                <ProjectCard title={data[2].title} text={data[2].text} />
+              <div>
+                {convertedData}
               </div>
             </div>
           </div>
