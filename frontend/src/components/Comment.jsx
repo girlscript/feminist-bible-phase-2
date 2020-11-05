abeccac867a1fd4f60d4f2547342b2b2f38ed378
priyanshisharma <priyanshi417@gmail.com>
2020-11-05 20:06:14 +0530

    feat: add comment and merge

---
+++ frontend/src/components/Comment.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+//components
+import Avatar from "./Avatar.jsx";
+
+//images
+import avatar_image from "../images/placeholder-images/avatar.png";
+
+class Comment extends Component {
+    render() {
+        return(
+            <>
+                <div className="grid-x">
+                    <div className="cell medium-shrink">
+                        <div className="comment__img-container">
+                            <Avatar avatarSrc={avatar_image}/>
+                        </div>
+                    </div>
+                    <div className="cell medium-auto">
+                        <div className="comment__text-container">
+                            <p className="comment__title">
+                                Courtney Henry
+                            </p>
+                            <p className="comment__time">
+                                5hr
+                            </p>
+                            <br/>
+                            <br/>
+                            <p className="comment__text">
+                                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
+                                Velit officia consequat duis enim velit mollit. Exercitation veniam consequat 
+                                sunt nostrud amet.
+                            </p>
+                        </div>
+                        <a className="reply"> 
+                                Reply
+                        </a>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+export default Comment;
