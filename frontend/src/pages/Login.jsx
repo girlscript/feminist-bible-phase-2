62e0d4e713d3a2b468373dfa61505dda36979184
Praveen Kumar <pkspyder007@gmail.com>
2021-01-14 17:23:50 +0530

    Fix issue #159 (#167)

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import { Input, WithPasswordStrength } from "../components/input";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+
+const womenImg = require("../images/women.png").default;
+
+export default class Registration extends Component {
+  state = {
+    email: "",
+    password: "",
+    errors: {
+      email: "",
+      password: "",
+    },
+  };
+
+  registerHandler = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      ...prevState,
+      errors: { email: "", password: "" },
+    }));
+    // validation
+    const { email, password } = this.state;
+
+    // email
+    const emailTest = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailTest.test(email)) {
+      this.setState((prevState) => ({
+        ...prevState,
+        errors: { ...prevState.errors, email: "Email is not valid." },
+      }));
+      return;
+    }
+
+    // password
+    if (!password) {
+      this.setState((prevState) => ({
+        ...prevState,
+        errors: { ...prevState.errors, password: "Password cannot be empty" },
+      }));
+      return;
+    }
+    if (password.length < 6) {
+      this.setState((prevState) => ({
+        ...prevState,
+        errors: {
+          ...prevState.errors,
+          password: "Password length must be atleast 6 characters.",
+        },
+      }));
+      return;
+    }
+    this.loginUser();
+  };
+
+  changeHandler = (e) => {
+    e.persist();
+    this.setState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  loginUser = async (user) => {
+    // add user to backend
+    try {
+      const {
+        email,
+        password,
+      } = this.state;
+      const res = await Axios({
+        url: "/api/auth/signin",
+        method: "POST",
+        data: {
+          email,
+          password,
+        },
+      });
+      alert("LoggedIN...");
+    } catch (error) {
+      alert(`Something went wrong! \n ${error.response.data.msg}`);
+    }
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <>
+        <div className="registration-page">
+          <div className="grid-container">
+            <div className="grid-x align-middle">
+              <div className="cell large-4 medium-4">
+                <h2 className="registration-page__title">Login</h2>
+                <form
+                  onSubmit={this.registerHandler}
+                  className="registration-page__form"
+                >
+                  <Input
+                    label="Email Address"
+                    type="email"
+                    name="email"
+                    placeholder="username@example.com"
+                    onChange={this.changeHandler}
+                    error={errors.email}
+                  />
+                  <Input
+                    label="Create Password"
+                    type="password"
+                    placeholder="************"
+                    name="password"
+                    onChange={this.changeHandler}
+                    error={errors.password}
+                  />
+                  <button className="button__big">Login</button>
+                </form>
+                <Link>
+                  Don't have an account?{" "}
+                  <span style={{ color: "#2F80ED" }}><Link to="/register">Register here</Link></span>.
+                </Link>
+              </div>
+              <div className="cell large-8">
+                <div className="registration-page__illustration-wrapper">
+                  <img
+                    src={womenImg}
+                    className="registration-page__illustration"
+                    alt="Illustration of women"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
