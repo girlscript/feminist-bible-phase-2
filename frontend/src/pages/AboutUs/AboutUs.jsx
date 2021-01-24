0dd51440cb6b2e81b9b4a26b6cf9c596233eb9d2
Sonali Singh <sonali122000@gmail.com>
2021-01-24 15:54:43 +0530

    format code with prettier

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -56,67 +56,69 @@ export class AboutUs extends Component {
 
                 <div className="cell medium-6">
                   <div className={style["coFounder-div"]}>
-                  <div className={style["coFounder-avatar"]}>
-                  <img src={img_1} alt="avatar" />
+                    <div className={style["coFounder-avatar"]}>
+                      <img src={img_1} alt="avatar" />
+                    </div>
+                    <div className={style["coFounder-name"]}>
+                      <h3>Maybelyn H. Plecic</h3>
+                      {/* <img src={linkedIn} alt="linkedIn" /> */}
+                    </div>
+                    <ul className={style["coFounder-details__list"]}>
+                      <li>Presidio Distinguished Engineer - 2019</li>
+                      <li>
+                        Professional Services Manager - 10 Reporting Engineers
+                      </li>
+                      <li>Cisco DevNet Creator Award 2019</li>
+                      <li>
+                        Cisco Certified - CCNA, CCNP, CCDA, CCDP - Collaboration
+                        Focus
+                      </li>
+                      <li>
+                        Lean In Wonder Women and Lean In Too Regional Leader
+                      </li>
+                      <li>The Star In Me Influencer</li>
+                      <li>Women Who Code Python Evangelist</li>
+                      <li>Podcast Host of Vitamae https://vitamae.org</li>
+                      <li>Blogger at https://maybelynplecic.com</li>
+                      <li>
+                        {" "}
+                        She is married to Jesenko Plecic, a life partner and
+                        mother of four boys 18 yrs. old, 15 years old, and 14
+                        years old twins
+                      </li>
+                    </ul>
                   </div>
-                  <div className={style["coFounder-name"]}>
-                    <h3>Maybelyn H. Plecic</h3>
-                    {/* <img src={linkedIn} alt="linkedIn" /> */}
-                  </div>
-                  <ul className={style["coFounder-details__list"]}>
-                    <li>Presidio Distinguished Engineer - 2019</li>
-                    <li>
-                      Professional Services Manager - 10 Reporting Engineers
-                    </li>
-                    <li>Cisco DevNet Creator Award 2019</li>
-                    <li>
-                      Cisco Certified - CCNA, CCNP, CCDA, CCDP - Collaboration
-                      Focus
-                    </li>
-                    <li>
-                      Lean In Wonder Women and Lean In Too Regional Leader
-                    </li>
-                    <li>The Star In Me Influencer</li>
-                    <li>Women Who Code Python Evangelist</li>
-                    <li>Podcast Host of Vitamae https://vitamae.org</li>
-                    <li>Blogger at https://maybelynplecic.com</li>
-                    <li>
-                      {" "}
-                      She is married to Jesenko Plecic, a life partner and
-                      mother of four boys 18 yrs. old, 15 years old, and 14
-                      years old twins
-                    </li>
-                  </ul>
-                </div>
                 </div>
 
                 <div className="cell medium-6 ">
                   <div className={style["coFounder-div"]}>
-                  <div className={style["coFounder-avatar"]}>
-                  <img src={img_2} alt="avatar" />
+                    <div className={style["coFounder-avatar"]}>
+                      <img src={img_2} alt="avatar" />
+                    </div>
+                    <div className={style["coFounder-name"]}>
+                      <h3>Rashmeet Kaur</h3>
+                      {/* <img src={linkedIn} alt="linkedIn" /> */}
+                    </div>
+                    <ul className={style["coFounder-details__list"]}>
+                      <li>Supply Chain Professional</li>
+                      <li>Mechanical and Manufacturing Systems Engineer</li>
+                      <li>Graduate Researcher at Cranfield University, UK</li>
+                      <li>AMPMO Fellow at Indian School of Business</li>
+                      <li>
+                        Lean In India and India Corporate Network- Co-Founder
+                      </li>
+                      <li>Pencils &amp; Pistons - Founder</li>
+                      <li>
+                        Strategic Programs and Partnership Consultant at The
+                        Star In Me
+                      </li>
+                      <li>
+                        Author - Diary of a Young Blogger and Colored Lens
+                      </li>
+                      <li>Podcaster- Unsolicited Supply ! and Unmuted</li>
+                      <li> Google IAmRemarkable Facilitator</li>
+                    </ul>
                   </div>
-                  <div className={style["coFounder-name"]}>
-                    <h3>Rashmeet Kaur</h3>
-                    {/* <img src={linkedIn} alt="linkedIn" /> */}
-                  </div>
-                  <ul className={style["coFounder-details__list"]}>
-                    <li>Supply Chain Professional</li>
-                    <li>Mechanical and Manufacturing Systems Engineer</li>
-                    <li>Graduate Researcher at Cranfield University, UK</li>
-                    <li>AMPMO Fellow at Indian School of Business</li>
-                    <li>
-                      Lean In India and India Corporate Network- Co-Founder
-                    </li>
-                    <li>Pencils &amp; Pistons - Founder</li>
-                    <li>
-                      Strategic Programs and Partnership Consultant at The Star
-                      In Me
-                    </li>
-                    <li>Author - Diary of a Young Blogger and Colored Lens</li>
-                    <li>Podcaster- Unsolicited Supply ! and Unmuted</li>
-                    <li> Google IAmRemarkable Facilitator</li>
-                  </ul>
-                </div>
                 </div>
               </div>
             </div>
@@ -126,5 +128,3 @@ export class AboutUs extends Component {
     );
   }
 }
-
-
