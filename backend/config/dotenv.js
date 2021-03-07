abeca1d7db28852af68d64eacc4c4fe5a9c4bb13
Gaurav Das <gauravdas014@gmail.com>
2021-03-07 13:23:50 +0530

    chore: remove check for .env

--- backend/config/dotenv.js
+++ backend/config/dotenv.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const path = require('path');
 
 // check if .env exists
-if (!fs.existsSync(path.resolve('.env'))) {
-  throw new TypeError('.env File Not Found. Forgot to create one?');
-}
+// if (!fs.existsSync(path.resolve('.env'))) {
+//   throw new TypeError('.env File Not Found. Forgot to create one?');
+// }
 
-require('dotenv').config({ path: path.resolve('.env') });
+// require('dotenv').config({ path: path.resolve('.env') });
