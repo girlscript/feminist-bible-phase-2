d1a0be6991713f9c9da5eeebfc2d4043b4d51658
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2021-03-25 16:09:39 +0530

    feat: connect DB before server start

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const app = require('./app');
+const mongodbConnect = require("./database/index")
 
 const port = process.env.PORT || 8080;
 
-// this is useful when creating a https server
-http
-  .createServer(app)
-  .listen(port, () => console.log('HTTP server listening on ' + port));
+(async () => {
+  
+  // Connect to DB
+  // const db = await mongodbConnect();
+
+  // this is useful when creating a https server
+  http
+    .createServer(app)
+    .listen(port, () => console.log('HTTP server listening on ' + port));
+
+})()
