f16f76c77933e7a3f795c25e28b7455299415f3e
Priyanshi Sharma <priyanshi417@gmail.com>
2021-03-27 16:03:30 +0530

    feat: add forum route

---
+++ frontend/src/pages/Forum/Forum.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+//Components
+import { ForumPost } from "../../components/ForumPost/index";
+
+export class Forum extends Component {
+  render() {
+    return (
+      <>
+       <ForumPost/>
+      </>
+    );
+  }
+}
