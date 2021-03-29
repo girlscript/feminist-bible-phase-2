9f24666a6743da090ea9b24fd2c1e33e0598f21b
singhayushh <coding.ayush@gmail.com>
2021-03-29 11:08:06 +0530

    ci: separate development and production env

--- backend/config/dotenv.js
+++ backend/config/dotenv.js
@@ -1,2 +1,24 @@
+const fs = require('fs');
 const path = require('path');
-require('dotenv').config({ path: path.resolve('.env') });
+
+// check if environment is set as development
+if (process.env.NODE_ENV === "development") {
+    // check if .env file exists
+    if (!fs.existsSync(path.resolve('.env'))) {
+        throw new TypeError('.env File Not Found. Forgot to create one?');
+    }
+
+    // load the .env file (for development)
+    require('dotenv').config({ path: path.resolve(`.env`) });
+} 
+
+// default the environment to production
+else {
+    // check if .env.production file exists
+    if (!fs.existsSync(path.resolve('.env.production'))) {
+        throw new TypeError('.env File Not Found. Forgot to create one?');
+    }
+
+    // load the .env.production file (for production)
+    require('dotenv').config({ path: path.resolve(`.env.production`) });
+}
